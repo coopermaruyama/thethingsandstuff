8a6e339378dc167eacefa2bc52217db57946ca5b
Cooper Maruyama <coopermaruyama@ap.local>
2014-01-22 16:51:30 -0800

    ishu coupon code

--- app/code/core/Mage/Checkout/controllers/OnepageController.php
+++ app/code/core/Mage/Checkout/controllers/OnepageController.php
@@ -588,4 +588,17 @@ class Mage_Checkout_OnepageController extends Mage_Checkout_Controller_Action
             || Mage::helper('checkout')->isAllowedGuestCheckout($this->getOnepage()->getQuote())
             || !Mage::helper('checkout')->isCustomerMustBeLogged();
     }
+	
+	function couponAction() 
+	{
+		  $this->loadLayout('checkout_onepage_review');
+		  $this->couponCode = (string) $this->getRequest()->getParam('coupon_code');
+		  Mage::getSingleton('checkout/cart')->getQuote()->getShippingAddress()->setCollectShippingRates(true);
+		  Mage::getSingleton('checkout/cart')->getQuote()->setCouponCode(strlen($this->couponCode) ?$this->couponCode : ' ')->collectTotals()->save();
+		  $result['goto_section'] = 'review';
+		  $result['update_section'] = array( 'name' => 'review', 'html' => $this->_getReviewHtml() );
+		  $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
+
+	}
+
 }
