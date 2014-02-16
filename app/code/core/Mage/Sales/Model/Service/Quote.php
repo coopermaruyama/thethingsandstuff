90c4a8b678e99ea1c046aa0e2012212770f09c1b
Cooper Maruyama <cooper@landersoptimized.com>
2014-02-16 00:28:44 -0800

    change invalid shipping method message to say 'invalid address' because the custom shipping module returns the wrong message. kinda hacky but deal with it

--- app/code/core/Mage/Sales/Model/Service/Quote.php
+++ app/code/core/Mage/Sales/Model/Service/Quote.php
@@ -300,7 +300,7 @@ class Mage_Sales_Model_Service_Quote
             $method= $address->getShippingMethod();
             $rate  = $address->getShippingRateByCode($method);
             if (!$this->getQuote()->isVirtual() && (!$method || !$rate)) {
-                Mage::throwException(Mage::helper('sales')->__('Please specify a shipping method.'));
+                Mage::throwException(Mage::helper('sales')->__('Invalid shipping address - please go back and enter a valid shipping address.'));
             }
         }
 
