dfcefbbb71b08f8152f6ac3b1f2e15fec0426530
Cooper Maruyama <cooper@landersoptimized.com>
2014-05-01 01:56:31 -0700

    after updating editing/saving

--- static/report/api.php
+++ static/report/api.php
@@ -9,14 +9,19 @@ extract($_POST);
 $conn = new PDO('mysql:host=205.186.146.64;dbname=revenue_report', 'root', '3120MacT!');
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if ($action == "unlock" && isset($row_id)) {
+if ($action == "unlock_row" && isset($row_id)) {
     $query = $conn->prepare("UPDATE `revenue_report`.`revenue_records` SET locked=0 WHERE `revenue_records`.`id`=$row_id");
     $query->execute();
     echo "success";
-} else if ($action == "lock" && isset($row_id)) {
+} else if ($action == "lock_row" && isset($row_id)) {
     $query = $conn->prepare("UPDATE `revenue_report`.`revenue_records` SET locked=1 WHERE `revenue_records`.`id`=$row_id");
     $query->execute();
     echo "success";
+} else if ($action == "update_column" && isset($row_id) && isset($column_name) && isset($new_value)) {
+	$query = $conn->prepare("UPDATE `revenue_report`.`revenue_records` SET `$column_name`='$new_value' WHERE `revenue_records`.`id`=$row_id");
+	$query->execute();
+	echo "success";
+
 }
 
 ?>
