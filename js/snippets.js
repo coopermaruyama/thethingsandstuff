33c1aeb67d352de696173ec2832878c9f2097b57
Cooper Maruyama <cooper@landersoptimized.com>
2015-03-23 08:18:55 -0700

    todos

--- js/snippets.js
+++ js/snippets.js
@@ -12,3 +12,14 @@ jQuery(".products-grid >.item").each(function(i,v) {
 
 /*-----  End of Check if infinite scroll created any duplicates         ------*/
 
+/**
+ * TODO: 1.Basic site functionality fixes
+ * TODO: Make sure pictures are working on all browsers at all zoom levels
+ * TODO: Make sure order emails don’t have typos
+ * TODO: Make sure filters and sorting are working
+ * TODO: Make sure payment error messages are accurate
+ * TODO: Duplicate items showing in grid still
+ * TODO: New Items category displaying out of stock items – THIS IS NEW PROJECT
+ * TODO: “View product categories” is being covered by the banner
+ * TODO: Long description fix (see description of  product #3267)
+ */
